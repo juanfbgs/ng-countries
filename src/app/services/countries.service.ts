214684ace7b685e969045f4bf23be6edd8be8cde
juanfbgs <juanfrburgos@gmail.com>
2026-05-31 15:53:16 -0300

    country service and base components created

---
+++ src/app/services/countries.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@envs/environment.development';
+import { Country } from '../models/country.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountriesService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = `${environment.API_URL}`;
+
+  getCountries(): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiUrl}/all?fields=name,capital,flags`);
+  }
+}
